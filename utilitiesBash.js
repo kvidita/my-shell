793049dea46ea49ae40d711c1dc7a822c91d97d2
Vidita Kumari <kvidita@users.noreply.github.com>
2023-05-02 00:29:56 +0530

    changed the arguments of commands to be array rather than string

--- utilitiesBash.js
+++ utilitiesBash.js
@@ -1,20 +1,38 @@
 const fs = require('fs');
 
+const isValidPath = function(path) {
+  return fs.existsSync(path);
+}
+
 const pwd = function(PWD, args) {
-  if(args === '') {
-    const presentWorkingDir = PWD;
-    return {pwd: presentWorkingDir, output: presentWorkingDir};
+  if(args.length >= 1) {
+    return {pwd: PWD, output: "pwd: too many arguments"}
   }
+  return {pwd: PWD, output: PWD};
 };
 
 const ls = function(PWD, args) {
-  const entries = (fs.readdirSync(PWD + '/' + args));
-  return {pwd: PWD, output: entries};
+  if(args.length >= 1) {
+    const path = PWD + '/' + args[0];
+    if(isValidPath(path)) {
+      const entries = (fs.readdirSync(PWD + '/' + args[0]));
+      return {pwd: PWD, output: entries};
+    }
+    return {pwd: PWD, output: "ls: no such file or directory"};
+  }
+  return {pwd: PWD, output: fs.readdirSync(PWD)};
 };
 
 const cd = function(PWD, args) {
-  const changedDir = PWD + '/' + args;
-  return {pwd: changedDir};
+  if(args.length > 1) {
+    return {pwd: PWD, output: "cd: too many arguments"};
+  }
+  const path = PWD + '/' + args[0];
+  if(isValidPath(path)) {
+    const changedDir = PWD + '/' + args[0];
+    return {pwd: changedDir};
+  }
+  return {pwd: PWD, output: "cd: no such file or directory"};
 };
 
 const command = {
@@ -23,12 +41,9 @@ const command = {
   cd: cd
 };
 
-const commandsPropertries = function(script) {
-  const commands = script.split(' ');
-  const commandProperties = {
-    name: commands[0], 
-    args: commands.slice(1).join(' ')
-  };
+const commandsProperties = function(commands) {
+  const [name, ...args] = commands.split(' ');
+  const commandProperties = {name, args};
 
   return commandProperties;
 }
@@ -37,7 +52,7 @@ const executeInstruction  = function(instruction, args, PWD) {
   return executedCommands = command[instruction](PWD, args);
 }
 
-const displayOutput = function(outputs) {
+const filterOutput = function(outputs) {
   const output = [];
 
   for(let commandOutput of outputs) {
@@ -51,13 +66,13 @@ const execute = function(instructions) {
   const commandOutput = [];
 
   for(const instruction of instructions) {
-    const command = commandsPropertries(instruction);
+    const command = commandsProperties(instruction);
     const processOutput = executeInstruction(command.name, command.args, PWD);
     PWD = processOutput.pwd;
     commandOutput.push(processOutput.output);
   }  
 
-  console.log(displayOutput(commandOutput));
+  console.log(filterOutput(commandOutput));
 }
 
 exports.execute = execute;
